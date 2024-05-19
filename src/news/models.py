d778fba5e55b597a4882d401809a3277f6d1fd81
Eduard Gumen <hydrospirt@mail.ru>
2024-05-19 13:09:52 +0300

    feat: Add News model

--- src/news/models.py
+++ src/news/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 
+class NewsCfg:
+    """
+    Настройки для модели News
+    """
+    MAX_LENGTH_DEFAULT = 255
+    NEWS_MAX_LENGHT = 5000
+    HELP_MSG_NAME = 'Введите название Новости'
+    HELP_MSG_TXT = f'Напишите текст новости до {NEWS_MAX_LENGHT} символов'
+
+
 class News(models.Model):
     name = models.CharField(
-        max_length=255,
+        max_length=NewsCfg.MAX_LENGTH_DEFAULT,
         verbose_name='Новости',
+        help_text=NewsCfg.HELP_MSG_NAME
     )
     image = models.ImageField(
         upload_to='media',
@@ -12,8 +23,8 @@ class News(models.Model):
         blank=True,
         verbose_name='Изображение'
     )
-    url_image = models.SlugField(
-        max_length=255,
+    url_image = models.URLField(
+        max_length=NewsCfg.MAX_LENGTH_DEFAULT,
         unique=True,
         verbose_name='Ссылка на изображение')
     date = models.DateTimeField(
@@ -21,4 +32,11 @@ class News(models.Model):
         verbose_name='Дата',)
     text = models.TextField(
         verbose_name='Содержание новости',
+        max_length=NewsCfg.NEWS_MAX_LENGHT,
+        help_text=NewsCfg.HELP_MSG_TXT
     )
+
+    class Meta:
+        ordering = ('date',)
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'

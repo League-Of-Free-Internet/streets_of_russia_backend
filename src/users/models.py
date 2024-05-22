8bae993497c21b1e09c743dfbf9267386030808c
Eduard Gumen <hydrospirt@mail.ru>
2024-05-22 21:03:35 +0300

    feat: Add CustomUserManager and CustomUser

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,73 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Создает и сохраняет пользователя,
+        нормализуетет email и сохраняет пароль.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """Специализированая модель пользователя с расширенными полями"""
+    first_name = models.CharField(
+        _('name'),
+        max_length=50,
+    )
+    last_name = models.CharField(
+        _('surname'),
+        max_length=50,
+    )
+    phone_number = PhoneNumberField(
+        _('phone number'),
+        unique=True,
+        null=False,
+        blank=False)
+    email = models.EmailField(
+        _('email'),
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """
+        Возвращает имя и фамилию с пробелом между ними.
+        """
+        full_name = f'{self.first_name} {self.last_name}'.title()
+        return full_name.strip()

01f17609dc37ecbf29666ae035e2c7a70b3947e4
Eduard Gumen <hydrospirt@mail.ru>
2024-05-22 21:23:36 +0300

    feat: Add CustomUserAdmin

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """
+    Обеспечивает отображение, фильтрацию и возможности поиска
+    в панели администратора для модели Пользователя.
+    """
+    list_display = (
+        "id",
+        "email",
+        "get_full_name",
+        "phone_number",
+        "is_active",
+        "is_staff",
+    )
+    list_filter = ("is_active",
+                   "is_staff",)
+    search_fields = ("get_full_name",
+                     "phone_number",
+                     "email")
+    CustomUser.get_full_name.short_description = "Имя и Фамилия"

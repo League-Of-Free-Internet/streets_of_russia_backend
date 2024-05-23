e6a50608fa503c1df50244c344f75a95503578c3
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-05-23 16:53:37 +0300

    fix: constants.py, models fields

--- src/core/constants.py
+++ src/core/constants.py
@@ -16,12 +16,34 @@ class EventsCfg:
     """
     Настройки для модели Events.
     """
-
-    EVENTS_HELP_MSG_NAME = "Введите название события"
-    EVENTS_HELP_MSG_TXT = (
+    EVENTS = "Events"
+    EVENTS_NAME_VERBOSE_NAME = "Название события"
+    EVENTS_NAME_HELP_MSG = "Введите название события"
+    EVENTS_DESCRIPTION_VERBOSE_NAME = "Описание события"
+    EVENTS_DESCRIPTION_HELP_MSG = (
         f"Напишите текст о событии до {MAX_LENGTH} символов"
     )
-    EVENTS_HELP_MSG_IMG = "Добавьте ссылки на изображения с событиями"
+    EVENTS_IMG_RELATED_NAME = "events_images"
+    EVENTS_IMG_URLS_VERBOSE_NAME = "Изображения для события"
+    EVENTS_IMG_URLS_HELP_MSG = "Добавьте ссылки на изображения с событиями"
+    EVENTS_PUB_DATE_VERBOSE_NAME = "Дата события"
+    EVENTS_PUB_DATE_HELP_MSG = "Выберите дату события"
+    EVENTS_PLACE_VERBOSE_NAME = "Место события"
+    EVENTS_PLACE_HELP_MSG = "Введите название места события"
+    EVENTS_DISCIPLINE_VERBOSE_NAME = "Дисциплина для события"
+    EVENTS_DISCIPLINE_HELP_MSG = "Выберите дисциплину для события"
+    EVENTS_META_VERBOSE_NAME = "Событие"
+    EVENTS_META_VERBOSE_NAME_PLURAL = "События"
+
+
+class EventsImageURLCfg:
+    EVENTS_IMAGE_URL = "EventsImageURL"
+    EVENTS_IMG_URL_FOREIGN_RELATED_NAME = "events"
+    EVENTS_IMG_URL_FOREIGN_VERBOSE_NAME = "События"
+    EVENTS_IMG_URL_VERBOSE_NAME = "Ссылка на изображение события"
+    EVENTS_IMG_URL_HELP_MSG = "Укажите URL-адрес изображения о событии"
+
+
 
 
 class SportsCfg:
--- src/events/models.py
+++ src/events/models.py
@@ -1,53 +1,65 @@
 from django.db import models
 from django.utils.safestring import mark_safe
 
-from core.constants import MAX_LENGTH_DEFAULT, EventsCfg, MAX_LENGTH
+from core.constants import MAX_LENGTH_DEFAULT, EventsCfg, EventsImageURLCfg, MAX_LENGTH
 
 
 class Events(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(
         max_length=MAX_LENGTH_DEFAULT,
-        verbose_name="Событие",
-        help_text=EventsCfg.EVENTS_HELP_MSG_NAME,
+        verbose_name=EventsCfg.EVENTS_NAME_VERBOSE_NAME,
+        help_text=EventsCfg.EVENTS_NAME_HELP_MSG,
+    )
+    description = models.TextField(
+        verbose_name=EventsCfg.EVENTS_DESCRIPTION_VERBOSE_NAME,
+        max_length=MAX_LENGTH,
+        help_text=EventsCfg.EVENTS_DESCRIPTION_HELP_MSG,
     )
     image_urls = models.ManyToManyField(
-        "EventsImageURL",
-        related_name="events_images",
-        verbose_name="Изображения для событий",
-        help_text=EventsCfg.EVENTS_HELP_MSG_IMG,
+        EventsImageURLCfg.EVENTS_IMAGE_URL,
+        related_name=EventsCfg.EVENTS_IMG_RELATED_NAME,
+        verbose_name=EventsCfg.EVENTS_IMG_URLS_VERBOSE_NAME,
+        help_text=EventsCfg.EVENTS_IMG_URLS_HELP_MSG,
     )
     pub_date = models.DateTimeField(
         auto_now_add=True,
-        verbose_name="Дата события"
+        verbose_name=EventsCfg.EVENTS_PUB_DATE_VERBOSE_NAME,
+        help_text=EventsCfg.EVENTS_PUB_DATE_HELP_MSG
     )
-    text = models.TextField(
-        verbose_name="Содержание события",
+    place = models.CharField(
+        verbose_name=EventsCfg.EVENTS_PLACE_VERBOSE_NAME,
         max_length=MAX_LENGTH,
-        help_text=EventsCfg.EVENTS_HELP_MSG_TXT,
+        help_text=EventsCfg.EVENTS_PLACE_HELP_MSG
     )
+
+    # discipline = models.ForeignKey(
+    #   "Disciplines",
+    #   verbose_name=EventsCfg.EVENTS_DISCIPLINE_VERBOSE_NAME,
+    #   help_text=EventsCfg.EVENTS_DISCIPLINE_HELP_MSG
+    # )
 
     class Meta:
         ordering = ("-pub_date",)
-        verbose_name = "Событие"
-        verbose_name_plural = "События"
+        verbose_name = EventsCfg.EVENTS_META_VERBOSE_NAME
+        verbose_name_plural = EventsCfg.EVENTS_META_VERBOSE_NAME_PLURAL
 
     def __str__(self) -> str:
         return self.name[:25]
 
 
 class EventsImageURL(models.Model):
-    news = models.ForeignKey(
-        "Events",
+    events = models.ForeignKey(
+        EventsCfg.EVENTS,
         on_delete=models.CASCADE,
-        related_name="events",
-        verbose_name="События",
+        related_name=EventsImageURLCfg.EVENTS_IMG_URL_FOREIGN_RELATED_NAME,
+        verbose_name=EventsImageURLCfg.EVENTS_IMG_URL_VERBOSE_NAME,
     )
     image_url = models.URLField(
         max_length=MAX_LENGTH_DEFAULT,
         unique=True,
-        verbose_name="Ссылка на изображение события",
-        help_text="Укажите URL-адрес изображения о событии",
+        verbose_name=EventsImageURLCfg.EVENTS_IMG_URL_VERBOSE_NAME,
+        help_text=EventsImageURLCfg.EVENTS_IMG_URL_HELP_MSG,
     )
 
     def image_tag(self):

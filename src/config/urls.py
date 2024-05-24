2a59cccf148c9ec30a1ee837661b92c4cd5d37f1
Eduard Gumen <hydrospirt@mail.ru>
2024-05-24 23:38:54 +0300

    fix: Fix the quotes

--- src/config/urls.py
+++ src/config/urls.py
@@ -22,8 +22,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        'api/',
-        include('api.urls')
+        "api/",
+        include("api.urls")
     ),
     path(
         "swagger<format>/",

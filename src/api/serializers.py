514a1a9c036511bb716b6970ac1dcb600918c9bd
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-05-25 11:32:03 +0300

    feat: events viewset, events serializer

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,25 +1,58 @@
 from rest_framework import serializers
 
+from events.models import Events, EventsImageURL
+from news.models import ImageURL, News
 from users.models import CustomUser
-from news.models import News, ImageURL
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Сериализатор пользователей"""
+    """Сериализатор пользователей."""
+
     class Meta:
         fields = "__all__"
         model = CustomUser
 
 
 class ImageURLSerializer(serializers.ModelSerializer):
+    """Сериализатор для url-ссылок изображений для новостей."""
+
     class Meta:
-        fields = ("image_url", )
+        fields = ("image_url",)
         model = ImageURL
 
 
 class NewsSerializer(serializers.ModelSerializer):
+    """Сериализатор новостей."""
+
     image_urls = ImageURLSerializer(many=True)
 
     class Meta:
         model = News
         fields = ("id", "name", "description", "image_urls")
+
+
+class EventsImageURLSerializer(serializers.ModelSerializer):
+    """Сериализатор для url-ссылок изображений для событий."""
+
+    class Meta:
+        model = EventsImageURL
+        fields = ("image_url",)
+
+
+class EventsSerializer(serializers.ModelSerializer):
+    """Сериализатор событий."""
+
+    image_urls = EventsImageURLSerializer(many=True)
+
+    class Meta:
+        model = Events
+        fields = (
+            "id",
+            "name",
+            "description",
+            "image_urls",
+            "start_date",
+            "place",
+            "rules",
+            "deadline_registration_date"
+        )

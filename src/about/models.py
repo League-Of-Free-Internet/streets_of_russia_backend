ae029be07b5b6fc96d506288001cabbacb152afe
Eduard Gumen <hydrospirt@mail.ru>
2024-05-26 16:06:47 +0300

    feat: Update constants OurMember model

--- src/about/models.py
+++ src/about/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from core.constants import (MAX_LENGTH, MAX_LENGTH_DEFAULT, MAX_LIST_LENGTH,
-                            AboutCfg, BannerVideoCfg)
+                            AboutCfg, BannerVideoCfg, OurMemberCfg)
 
 
 class BannerVideo(models.Model):
@@ -21,34 +21,34 @@ class BannerVideo(models.Model):
 class OurMembers(models.Model):
     id = models.AutoField(primary_key=True)
     role = models.ForeignKey(
-        "MemberRole",
+        OurMemberCfg.ROLE_MODEL,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name="members",
-        verbose_name="Роль",
+        related_name=OurMemberCfg.ROLE_RELATED_NAME,
+        verbose_name=OurMemberCfg.ROLE_VERBOSE_NAME,
         help_text=AboutCfg.HELP_MSG_ROLE,
     )
     image_url = models.URLField(
         max_length=MAX_LENGTH_DEFAULT,
         unique=True,
-        verbose_name="Ссылка на изображение",
-        help_text="Укажите URL-адрес изображения",
+        verbose_name=OurMemberCfg.IMAGE_URL_VERBOSE_NAME,
+        help_text=OurMemberCfg.IMAGE_URL_HELP_TEXT,
     )
     name = models.CharField(
         max_length=MAX_LENGTH_DEFAULT,
-        verbose_name="Имя",
+        verbose_name=OurMemberCfg.NAME_VERBOSE_NAME,
         help_text=AboutCfg.HELP_MSG_NAME,
     )
     text = models.TextField(
-        verbose_name="История участника",
+        verbose_name=OurMemberCfg.TEXT_VERBOSE_NAME,
         max_length=MAX_LENGTH,
-        help_text=AboutCfg.HELP_MSG_TXT,
+        help_text=OurMemberCfg.TEXT_HELP_MSG,
     )
 
     class Meta:
-        verbose_name = "Наш участник"
-        verbose_name_plural = "Наши участники"
+        verbose_name = OurMemberCfg.OUR_MEMBERS_VERBOSE_NAME
+        verbose_name_plural = OurMemberCfg.OUR_MEMBERS_VERBOSE_NAME_PLURAL
 
     def __str__(self) -> str:
         return self.name[:MAX_LIST_LENGTH]

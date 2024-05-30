2e164bce59619cc7198474fa02fac95b094068c6
Eduard Gumen <hydrospirt@mail.ru>
2024-05-31 00:17:17 +0300

    chore: Fix code style validators.py

--- src/core/validators.py
+++ src/core/validators.py
@@ -1,8 +1,10 @@
 import re
+
 import requests
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 from rest_framework import status
+
 from core.constants import MediaValidatorCfg
 
 
@@ -15,8 +17,8 @@ def validate_image_url(url):
         response = requests.head(url)
         content_type = response.headers.get(MediaValidatorCfg.CONTENT_TYPE)
         if response.status_code != status.HTTP_200_OK:
-            raise ValidationError(MediaValidatorCfg.ACCESS_ERR_MSG +
-                                  f"Ошибка {response.status_code}")
+            raise ValidationError(MediaValidatorCfg.ACCESS_ERR_MSG
+                                  + f"Ошибка {response.status_code}")
         if MediaValidatorCfg.IMAGE not in content_type:
             raise ValidationError(MediaValidatorCfg.URL_ERR_IMG)
     except requests.RequestException:
@@ -31,7 +33,8 @@ def validate_url_video(url):
     try:
         response = requests.head(url)
         content_type = response.headers.get(MediaValidatorCfg.CONTENT_TYPE)
-        if MediaValidatorCfg.VIDEO not in content_type and not re.match(MediaValidatorCfg.YT_REGEX, url):
+        if (MediaValidatorCfg.VIDEO not in content_type
+                and not re.match(MediaValidatorCfg.YT_REGEX, url)):
             raise ValidationError(MediaValidatorCfg.URL_ERR_VID)
     except requests.RequestException:
         raise ValidationError(MediaValidatorCfg.ACCESS_ERR_MSG)

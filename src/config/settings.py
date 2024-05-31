3b7f2a2a7d3184b2f94e6f4cea13abf28480a35c
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-05-31 18:31:20 +0300

    fix: settings.py double quotes

--- src/config/settings.py
+++ src/config/settings.py
@@ -65,22 +65,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-if strtobool(os.getenv('DEBUG', default="True")):
+if strtobool(os.getenv("DEBUG", default="True")):
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "default": {
+            "ENGINE": 'django.db.backends.sqlite3',
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': os.getenv('DB_ENGINE'),
-            'NAME': os.getenv('POSTGRES_DB'),
-            'USER': os.getenv('POSTGRES_USER'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT')
+        "default": {
+            "ENGINE": os.getenv('DB_ENGINE'),
+            "NAME": os.getenv('POSTGRES_DB'),
+            "USER": os.getenv('POSTGRES_USER'),
+            "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+            "HOST": os.getenv('DB_HOST'),
+            "PORT": os.getenv('DB_PORT')
         }
     }
 

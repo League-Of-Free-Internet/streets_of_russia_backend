44b4733c4a8b9513baaf4ab479383f9962bf780c
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-06-03 20:14:08 +0300

    fix: settings.py DATABASES

--- src/config/settings.py
+++ src/config/settings.py
@@ -99,12 +99,12 @@ else:
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_action_db',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 

4ab67ca126648fc3d47ac8165ce8b749ac2e041a
Eduard Gumen <hydrospirt@mail.ru>
2024-07-23 21:38:11 +0300

    feat: Add custom_exception_handler, not_found, server_error, bad_request, forbidden

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,3 +1,7 @@
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.views import exception_handler
+
 from core.constants import RELATED_NAME_MAP
 
 
@@ -14,3 +18,59 @@ def get_image_urls(obj):
         )
     image_urls = getattr(obj, related_name).all()
     return [url.image_url for url in image_urls]
+
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    if response is not None:
+        response.data["status_code"] = response.status_code
+        response.data["error_message"] = response.data.pop("detail", None)
+    return response
+
+
+def not_found(request, exception, *args, **kwargs):
+    """
+    Общий 404 обработчик ошибок.
+    """
+    data = {
+        "status_code": 404,
+        "error_message": "Запрашиваемый ресурс не найден (404)"
+    }
+    return JsonResponse(data, status=status.HTTP_404_NOT_FOUND,
+                        json_dumps_params={"ensure_ascii": False})
+
+
+def server_error(request, *args, **kwargs):
+    """
+    Общий 500 обработчик ошибок.
+    """
+    data = {
+        "status_code": 500,
+        "error": "Ошибка сервера (500)"
+    }
+    return JsonResponse(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        json_dumps_params={"ensure_ascii": False})
+
+
+def bad_request(request, exception, *args, **kwargs):
+    """
+    Общий 400 обработчик ошибок.
+    """
+    data = {
+        "status_code": 400,
+        "error": "Плохой запрос (400)"
+    }
+    return JsonResponse(data, status=status.HTTP_400_BAD_REQUEST,
+                        json_dumps_params={"ensure_ascii": False})
+
+
+def forbidden(request, exception, *args, **kwargs):
+    """
+    Общий 400 обработчик ошибок.
+    """
+    data = {
+        "status_code": 403,
+        "error": "Доступ запрещён (403)"
+    }
+    return JsonResponse(data, status=status.HTTP_403_FORBIDDEN,
+                        json_dumps_params={"ensure_ascii": False})

94925be607d4b6024d4934199bb92874506d1d96
Eduard Gumen <hydrospirt@mail.ru>
2024-07-23 21:35:05 +0300

    feat: Add EXCEPTION_HANDLER

--- src/config/settings.py
+++ src/config/settings.py
@@ -164,6 +164,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "EXCEPTION_HANDLER": "core.utils.custom_exception_handler",
 }
 
 SIMPLE_JWT = {

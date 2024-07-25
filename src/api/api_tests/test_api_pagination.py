255920cb9f40e6ba5e1e2e50d6d4b595fe76cc7a
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-07-25 15:35:22 +0300

    added: test_api_pagination.py

--- src/api/api_tests/test_api_pagination.py
+++ src/api/api_tests/test_api_pagination.py
@@ -12,17 +12,16 @@ class PaginationAPITest(APITestCase):
             news_batch=self.news_batch
         )
         self.response = self.client.get(reverse("news-list"))
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
 
     def tearDown(self):
         [news.delete() for news in self.news_batch]
         [image_urls.delete() for image_urls in self.image_urls_batch]
 
     def test_news_paginator_page_size(self):
-        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(self.response.data.get("news")), 12)
 
     def test_news_paginator_links(self):
-        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
         links = self.response.data.get("links")
         self.assertIn("next", links)
         self.assertIn("previous", links)
@@ -30,5 +29,26 @@ class PaginationAPITest(APITestCase):
         self.assertIsNone(links.get("previous"))
 
     def test_news_paginator_page_number(self):
-        self.assertEqual(self.response.status_code, 200)
         self.assertEqual(self.response.data.get("page_number"), 1)
+
+    def test_news_paginator_invalid_page(self):
+        self.assertEqual(
+            self.client.get(reverse("news-list") + "?page=1000").status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+    def test_news_paginator_total_count(self):
+        self.assertEqual(self.response.data.get("count"), 100)
+
+    def test_news_paginator_page_links(self):
+        response = self.client.get(self.response.data.get("links").get("next"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data.get("links").get("previous"))
+        self.assertEqual(response.data.get("page_number"), 2)
+
+    def test_news_paginator_last_page(self):
+        response = self.client.get(reverse("news-list") + "?page=9")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("news")), 4)
+        self.assertIsNone(response.data.get("links").get("next"))
+        self.assertIsNotNone(response.data.get("links").get("previous"))

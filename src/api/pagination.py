385124fb64ccbde75baa7b7f6d3cae241fdc7e6e
Eduard Gumen <hydrospirt@mail.ru>
2024-07-27 19:23:17 +0300

    feat: Add DisciplinesPagination

--- src/api/pagination.py
+++ src/api/pagination.py
@@ -9,11 +9,28 @@ class NewsPagination(pagination.PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
+            "links": {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link()
             },
             "count": self.page.paginator.count,
-            'page_number': self.page.number,
+            "page_number": self.page.number,
             "news": data
+        })
+
+
+class DisciplinesPagination(pagination.PageNumberPagination):
+    page_size = 12
+    page_size_query_param = "page_size"
+    max_page_size = 12
+
+    def get_paginated_response(self, data):
+        return Response({
+            "links": {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link()
+            },
+            "count": self.page.paginator.count,
+            "page_number": self.page.number,
+            "disciplines": data
         })
